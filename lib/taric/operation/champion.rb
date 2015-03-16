651a70d54014ff3831c48bc58418d743834fdc9d
Joseph Yi <jyi@spokeo.com>
2015-03-16 16:42:37 -0700

    testing documentation

--- lib/taric/operation/champion.rb
+++ lib/taric/operation/champion.rb
@@ -11,6 +11,16 @@ module Taric
       CHAMPIONS =  Addressable::Template.new("#{CHAMPION_BASE_URL}{?api_key,freeToPlay}")
       CHAMPION_BY_ID = Addressable::Template.new("#{CHAMPION_BASE_URL}/{id}{?api_key}")
 
+      # Returns [Hash] embedding an [Array] of champion data.
+      #
+      # @see {https://developer.riotgames.com/api/methods#!/958/3290}
+      # @param free_to_play [Boolean] nil returns all, true or false to filter if they're free to play or not
+      # @return [Hash] embedding [Array] of champions keyed off of "champions"
+      #
+      # @example
+      #   all_champions = client.champions["champions"]
+      #   free_champions = client.champions(free_to_play: true)["champions"]
+      #   nonfree_champions = client.champions(free_to_play: false)["champions"]
       def champions(free_to_play: nil)
         response_for CHAMPIONS, freeToPlay: free_to_play
       end
