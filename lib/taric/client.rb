200d874126244baee98b07f92c4967443d72469e
Joseph Yi <jyi@spokeo.com>
2015-03-17 13:08:21 -0700

    move region hash

--- lib/taric/client.rb
+++ lib/taric/client.rb
@@ -4,6 +4,20 @@ module Taric
   class Client
     include Taric::Operation::API
 
+    REGION_ENDPOINT_INFO = {
+        br: {region: 'br'.freeze, platform_id: 'BR1'.freeze, host: 'br.api.pvp.net'},
+        eune: {region: 'eune'.freeze, platform_id: 'EUN1'.freeze, host: 'eune.api.pvp.net'},
+        euw: {region: 'euw'.freeze, platform_id: 'EUW1', host: 'euw.api.pvp.net'},
+        kr: {region: 'kr'.freeze, platform_id: 'KR', host: 'kr.api.pvp.net'},
+        lan: {region: 'lan'.freeze, platform_id: 'LA1', host: 'lan.api.pvp.net'},
+        las: {region: 'las'.freeze, platform_id: 'LA2', host: 'las.api.pvp.net'},
+        na: {region: 'na'.freeze, platform_id: 'NA1', host: 'na.api.pvp.net'},
+        oce: {region: 'oce'.freeze, platform_id: 'OC1', host: 'oce.api.pvp.net'},
+        tr: {region: 'tr'.freeze, platform_id: 'TR1', host: 'tr.api.pvp.net'},
+        ru: {region: 'ru'.freeze, platform_id: 'RU', host: 'ru.api.pvp.net'},
+        pbe: {region: 'pbe'.freeze, platform_id: 'PBE1', host: 'pbe.api.pvp.net'}
+    }.freeze
+
     def initialize(api_key, region, requestor, response_handler)
       @api_key = api_key
       @region = region
@@ -14,7 +28,7 @@ module Taric
     class << self
       extend Memoist
       def operation_values(api_key:, region:)
-        {api_key: api_key}.merge!(Taric::Operation::API::REGION_ENDPOINT_INFO[region])
+        {api_key: api_key}.merge!(REGION_ENDPOINT_INFO[region])
       end
       memoize :operation_values
 
--- lib/taric/operation/api.rb
+++ lib/taric/operation/api.rb
@@ -22,20 +22,11 @@ module Taric
       include Taric::Operation::Stats
       include Taric::Operation::Summoner
 
-      REGION_ENDPOINT_INFO = {
-          br: {region: 'br'.freeze, platform_id: 'BR1'.freeze, host: 'br.api.pvp.net'},
-          eune: {region: 'eune'.freeze, platform_id: 'EUN1'.freeze, host: 'eune.api.pvp.net'},
-          euw: {region: 'euw'.freeze, platform_id: 'EUW1', host: 'euw.api.pvp.net'},
-          kr: {region: 'kr'.freeze, platform_id: 'KR', host: 'kr.api.pvp.net'},
-          lan: {region: 'lan'.freeze, platform_id: 'LA1', host: 'lan.api.pvp.net'},
-          las: {region: 'las'.freeze, platform_id: 'LA2', host: 'las.api.pvp.net'},
-          na: {region: 'na'.freeze, platform_id: 'NA1', host: 'na.api.pvp.net'},
-          oce: {region: 'oce'.freeze, platform_id: 'OC1', host: 'oce.api.pvp.net'},
-          tr: {region: 'tr'.freeze, platform_id: 'TR1', host: 'tr.api.pvp.net'},
-          ru: {region: 'ru'.freeze, platform_id: 'RU', host: 'ru.api.pvp.net'},
-          pbe: {region: 'pbe'.freeze, platform_id: 'PBE1', host: 'pbe.api.pvp.net'}
-      }.freeze
-
+      # Template for requesting the url and processing the response.
+      #
+      # @param url String
+      # @param requestor Proc (lambda)
+      # @param response_handler Proc (lambda)
       API_CALL = -> (url:, requestor:, response_handler:) {
         response_handler.(requestor.(url))
       }.curry
