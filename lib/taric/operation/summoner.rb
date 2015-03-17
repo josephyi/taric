26ac250ebf580a37a5d7b1df1e050bf5d0212a2a
Joseph Yi <dissonance@gmail.com>
2015-03-17 01:27:05 -0700

    fix method names for consistency, documentation, version bump

--- lib/taric/operation/summoner.rb
+++ lib/taric/operation/summoner.rb
@@ -6,30 +6,48 @@ module Taric
       
       VERSION = 'v1.4'
       BASE_SUMMONER_URL = "#{BASE_URL_FN.(VERSION)}/summoner"
-
       BY_NAMES =  Addressable::Template.new "#{BASE_SUMMONER_URL}/by-name/{summonerNames}{?api_key}"
-      BY_IDS = Addressable::Template.new "#{BASE_SUMMONER_URL}/{summonerIds}{?api_key}"
-      MASTERIES = Addressable::Template.new "#{BY_IDS}/masteries{?api_key}"
-      NAMES = Addressable::Template.new "#{BY_IDS}/name{?api_key}"
-      RUNES = Addressable::Template.new "#{BY_IDS}/rune{?api_key}"
+      BASE_SUMMONERS_BY_IDS = "#{BASE_SUMMONER_URL}/{summonerIds}"
+      SUMMONERS_BY_IDS = Addressable::Template.new "#{BASE_SUMMONERS_BY_IDS}{?api_key}"
+      MASTERIES = Addressable::Template.new "#{BASE_SUMMONERS_BY_IDS}/masteries{?api_key}"
+      NAMES = Addressable::Template.new "#{BASE_SUMMONERS_BY_IDS}/name{?api_key}"
+      RUNES = Addressable::Template.new "#{BASE_SUMMONERS_BY_IDS}/runes{?api_key}"
 
-      def summoners_by_names(names:)
-        response_for BY_NAMES, {summonerNames: names}
+      # Returns [Hash] of summoner data hashes keyed by summoner name
+      #
+      # @see {https://developer.riotgames.com/api/methods#!/960/3292}
+      # @param summoner_names [String] required, summoner names separated by commas
+      # @return [Hash] of summoner data hashes keyed by summoner name
+      #
+      # @example
+      #   summoners = client.summoners_by_name(summoner_names: 'orlyzomg,ipa,dbanksdesign,lzrface,doodiediddle')
+      #   summoner = summoners['orlyzomg']
+      def summoners_by_names(summoner_names:)
+        response_for BY_NAMES, {summonerNames: summoner_names}
       end
 
+      # Returns [Hash] of summoner data hashes keyed by summoner ID
+      #
+      # @see {https://developer.riotgames.com/api/methods#!/960/3293}
+      # @param summoner_ids [String] required, summoner IDs separated by commas
+      # @return [Hash] of summoner data hashes keyed by summoner ID
+      #
+      # @example
+      #   summoners = client.summoners_by_id(summoner_ids: '39497114,35035046,38332778,21066,38877656')
+      #   summoner = summoners['21066']
       def summoners_by_ids(summoner_ids:)
-        response_for BY_IDS, {summonerIds: summoner_ids}
+        response_for SUMMONERS_BY_IDS, {summonerIds: summoner_ids}
       end
 
-      def masteries(summoner_ids:)
+      def summoner_masteries(summoner_ids:)
         response_for MASTERIES, {summonerIds: summoner_ids}
       end
 
-      def name(summoner_ids)
-        response_for NAME, {summonerIds: summoner_ids}
+      def summoner_ids_to_names(summoner_ids:)
+        response_for NAMES, {summonerIds: summoner_ids}
       end
 
-      def runes(summoner_ids:)
+      def summoner_runes(summoner_ids:)
         response_for RUNES, {summonerIds: summoner_ids}
       end
 
--- lib/taric/version.rb
+++ lib/taric/version.rb
@@ -1,3 +1,3 @@
 module Taric
-  VERSION = "0.1.1"
+  VERSION = "0.1.2"
 end
