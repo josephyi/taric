cae26d96f600556b497d569d93967a9d2c49165b
Joseph Yi <jyi@spokeo.com>
2015-04-02 14:02:09 -0700

    #2 default to Faraday's default adapter (Net::HTTP) if no adapter is configured (typhoeus non-trivial on windows)

--- lib/taric/configuration.rb
+++ lib/taric/configuration.rb
@@ -14,7 +14,7 @@ module Taric
       @api_key = options.fetch(:api_key, ENV.fetch('RIOT_API_KEY'.freeze, nil))
       @format = options.fetch(:format, :json)
       @user_agent = options.fetch(:user_agent, 'Taric Gem')
-      @adapter = options.fetch(:adapter, :typhoeus)
+      @adapter = options.fetch(:adapter, Faraday.default_adapter)
       @region = options.fetch(:region, ENV.fetch('RIOT_API_REGION'.freeze, 'na').to_sym)
       @connection_opts = options.fetch(:connection_opts, {})
       @requestor = options.fetch(:requestor, DEFAULT_REQUESTOR)
