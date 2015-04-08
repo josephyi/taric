c430297e11b94d598ea3e91a0335ba105b7dcdc8
Joseph Yi <jyi@spokeo.com>
2015-04-08 14:32:08 -0700

    support parallel requests

--- lib/taric/client.rb
+++ lib/taric/client.rb
@@ -1,8 +1,11 @@
 require_relative 'operation/api'
 require 'memoist'
+require_relative 'connection'
 module Taric
   class Client
+    include Taric::Connection
     include Taric::Operation::API
+    attr_reader :api_key, :region, :conn
 
     REGION_ENDPOINT_INFO = {
         br: {region: 'br'.freeze, platform_id: 'BR1'.freeze, host: 'br.api.pvp.net'},
@@ -25,15 +28,23 @@ module Taric
     # @param requestor [Proc] lambda that will accept a url and return a [Faraday::Response]
     # @param response_handler [Proc] lambda that accepts [Faraday::Response] and handles it
     #
-    def initialize(api_key:, region:, requestor:, response_handler:)
+    def initialize(api_key:, region:, config:) #requestor:, response_handler:)
       raise ArgumentError, 'api_key cannot be nil' if api_key.nil?
       raise ArgumentError, 'region cannot be nil' if region.nil?
       raise ArgumentError, "#{region} is not a valid region, #{REGION_ENDPOINT_STRING_KEYS}" if REGION_ENDPOINT_INFO[region].nil?
 
       @api_key = api_key
       @region = region
-      @requestor = requestor
-      @response_handler = response_handler
+      @config = config
+      @conn = connection(config)
+      # @requestor = requestor
+      # @response_handler = response_handler
+    end
+
+    # Returns a ParallelClient that will execute operations in parallel.
+    # @return [ParallelClient]
+    def in_parallel
+      ParallelClient.new(self)
     end
 
     class << self
@@ -43,8 +54,8 @@ module Taric
       #
       # Sets up and returns hash of api key and region values.
       #
-      # @params api_key [String] rito api key
-      # @params region [Symbol] key for region
+      # @param api_key [String] rito api key
+      # @param region [Symbol] key for region
       # @return [Hash] of api_key and region info
       def operation_values(api_key:, region:)
         {api_key: api_key}.merge!(REGION_ENDPOINT_INFO[region])
@@ -69,8 +80,29 @@ module Taric
     private
     def response_for(operation, options = {})
       -> url {
-        API_CALL.(url: url, requestor: @requestor, response_handler: @response_handler)
+        API_CALL.(url: url, requestor: @config.requestor.(@connection), response_handler: @config.response_handler)
       }.(self.class.expand_template(api_key: @api_key, region: @region, operation: operation, options: options))
+    end
+
+    class ParallelClient
+      include Taric::Operation::API
+      def initialize(parent)
+        @parent = parent
+        @operations = []
+      end
+
+      def response_for(operation, options = {})
+        @operations << @parent.class.expand_template(api_key: @parent.api_key, region: @parent.region, operation: operation, options: options)
+        self
+      end
+
+      def execute!
+        responses = []
+        @parent.conn.in_parallel do
+          @operations.each_with_object([]){|op, arr| responses << @parent.conn.get(op)}
+        end
+        responses
+      end
     end
   end
 end
