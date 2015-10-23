5e36cfe5f89849e5dcb0834a0646af66fbe29614
Joseph Yi <dissonance@gmail.com>
2015-10-23 02:04:56 -0700

    fix specs

--- lib/taric/constants.rb
+++ lib/taric/constants.rb
@@ -1,5 +1,4 @@
 module Taric
-
   # rankedQueues
   RANKED_SOLO_5X5 = 'RANKED_SOLO_5x5'.freeze
   RANKED_TEAM_3X3 = 'RANKED_TEAM_3x3'.freeze
@@ -12,6 +11,4 @@ module Taric
   SEASON2014 = 'SEASON2014'.freeze
   PRESEASON2015 = 'PRESEASON2015'.freeze
   SEASON2015 = 'SEASON2015'.freeze
-
-  freeze
 end
